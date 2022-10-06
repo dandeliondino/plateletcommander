fea6f50e069b11b783a538ba7c877f02ca18f6fe
dandeliondino <68911895+dandeliondino@users.noreply.github.com>
2022-10-06 12:28:02 -0700

    Change passive_mover attempts from yield() to timer

--- entities/passive_mover.gd
+++ entities/passive_mover.gd
@@ -1,38 +1,43 @@
 extends Node2D
 
-export(float) var wait_time_if_stuck := 2.0
-export(int) var wait_loops_if_stuck := 10
-
+var move_attempts := 0
+var max_move_attempts := 20
+var wait_time_if_stuck := 2.0
 
 onready var entity : Entity = get_parent()
-
+onready var timer := Timer.new()
 
 func _ready() -> void:
 	entity.connect("movement_completed", self, "_on_Entity_movement_completed") 
 	entity.connect("became_clotted", self, "_on_Entity_became_clotted")
 	
+	_setup_timer()
+	
 	if !Game.started:
 		yield(Events, "level_loaded")
 	yield(get_tree(), "idle_frame")
-	move()
+	try_to_move()
 
 
-func move():
+func _setup_timer() -> void:
+	add_child(timer)
+	timer.connect("timeout", self, "_on_timer_timeout")
+	timer.one_shot = true
+	
+
+func try_to_move() -> void:
 	var target_cell = _get_next_target_cell()
-	if target_cell != Game.INVALID_CELL:
-		entity.move_to_cell(target_cell)
+	if target_cell == Game.INVALID_CELL:
+		move_attempts += 1
+		if move_attempts < max_move_attempts:
+			timer.start(wait_time_if_stuck)
+		else:
+			Dprint.warning("Entity stuck and cannot move, giving up")
 		return
 	
-	var i := 0
-	while target_cell == Game.INVALID_CELL:
-		i += 1
-		if i >= wait_loops_if_stuck:
-			Dprint.warning("Entity stuck and cannot move, giving up")
-			return
-		yield(get_tree().create_timer(wait_time_if_stuck), "timeout")
-		target_cell = _get_next_target_cell()
-	
+	move_attempts = 0
 	entity.move_to_cell(target_cell)
+	
 	
 
 func _get_next_target_cell() -> Vector2:
@@ -51,10 +56,15 @@ func _get_next_target_cell() -> Vector2:
 
 
 func _on_Entity_movement_completed() -> void:
-	move()
+	try_to_move()
+
 
 func _on_Entity_became_clotted() -> void:
 	queue_free()
+
+
+func _on_timer_timeout() -> void:
+	try_to_move()
 
 
 func _on_Area2D_area_entered(area: Area2D) -> void:
